5eaaf42221b7312d426c2e90a0e288e502aea3e5
Jonny Fairfull <jonnyboris@googlemail.com>
2011-10-21 19:59:15 +0100

    Added Unlimited account charge call and some Crude tests

--- src/uk/ac/ncl/coursework/phonepractice/UnlimitedAccount.java
+++ src/uk/ac/ncl/coursework/phonepractice/UnlimitedAccount.java
@@ -8,12 +8,71 @@ final class UnlimitedAccount extends PhoneAccountFactory {
 
 	@Override
 	public Call chargeCall(PhoneNumber toNumber, int duration) {
+		//TODO remove sysos
+		System.out.println("d " + duration);
+
 		if(toNumber == null) {
 			throw new IllegalArgumentException("toNumber cannot be null");
-		} else if ( duration < 0) {
-			throw new IllegalArgumentException("Call duration cannot be negative");
+		} else if ( duration <= 0) {
+			throw new IllegalArgumentException("Call duration cannot be negative or zero");
 		}  
-		return null;
-	}
+		
+		int bal = getBalance(), 
+			cost =0,
+			tempCost = 0,
+			balanceOverBoundary = 0,
+			timeOverBoundary = 0,
+			tempDuration = duration;
+		final Call call;
+		
+		if(bal > 1500) {
+			tempCost = 10 * tempDuration;
+			
+			if(bal - tempCost < 1500) {
+				balanceOverBoundary = bal - 1500;
+				timeOverBoundary = balanceOverBoundary / 10;
+				System.out.println("time over 15: " + timeOverBoundary);
+				System.out.println("balance over 15: " + balanceOverBoundary);
+				
+				cost += balanceOverBoundary;
+				bal -= balanceOverBoundary;
+				tempDuration -= timeOverBoundary;
+				
+				System.out.println("Duration mins left " + tempDuration);
 
+			} else {
+				cost += tempCost;
+			}
+		}
+		
+		if((bal > 0  && bal <= 1500) && tempDuration > 0) {
+			tempCost = 20 * tempDuration;
+			
+			if(bal - tempCost < 0 ) {
+				balanceOverBoundary = bal;
+				timeOverBoundary = balanceOverBoundary / 20;
+				System.out.println("time over 0: " + timeOverBoundary);
+				System.out.println("balance over 0: " + balanceOverBoundary);
+				
+				cost += balanceOverBoundary;
+				bal -= balanceOverBoundary;
+				tempDuration -= timeOverBoundary;
+				
+				System.out.println("Duration mins left " + tempDuration);
+	
+			} else {
+				cost += tempCost;
+			}
+		}
+		
+		if(bal <= 0 && tempDuration > 0) {
+			tempCost = tempDuration * 50;
+			cost += tempCost;
+		}
+		debit(cost);
+		System.out.println("Cost: " + cost + " Balance = "  + getBalance());
+		return new Call(toNumber, duration, cost);
+	}
+	
+	
 }
--- src/uk/ac/ncl/coursework/phonepractice/UsePhoneMan.java
+++ src/uk/ac/ncl/coursework/phonepractice/UsePhoneMan.java
@@ -3,12 +3,17 @@ package uk.ac.ncl.coursework.phonepractice;
 public class UsePhoneMan {
 	public static void main(String args[]) {
 		 
-		PhoneAccount pa = PhoneAccountFactory.getInstance(new PhoneNumber(191, 2815532), new Person(), PhoneAccountFactory.PAY_AS_YOU_GO);
+		PhoneAccount pa = PhoneAccountFactory.getInstance(new PhoneNumber(191, 2815532), new Person(), PhoneAccountFactory.UNLIMITED);
+		//System.out.println(pa.getBalance());
+		pa.credit(30);
 		System.out.println(pa.getBalance());
-		pa.credit(10);
-		System.out.println(pa.getBalance());
-		pa.chargeCall(new PhoneNumber(191, 2813255), 7);
-		System.out.println(pa.getBalance());
+		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
+		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
+		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
+		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
+		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
+		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
+		//System.out.println(pa.getBalance());
 	}
 
 }

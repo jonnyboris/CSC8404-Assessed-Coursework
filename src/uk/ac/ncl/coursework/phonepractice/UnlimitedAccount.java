32563cbc6bd17ad936b681cd546a3d81945b5340
jonnyboris <jonny@googlemail.com>
2011-10-23 00:38:54 +0100

    tidy up of charge call

--- src/uk/ac/ncl/coursework/phonepractice/UnlimitedAccount.java
+++ src/uk/ac/ncl/coursework/phonepractice/UnlimitedAccount.java
@@ -8,9 +8,6 @@ final class UnlimitedAccount extends PhoneAccountFactory {
 
 	@Override
 	public Call chargeCall(PhoneNumber toNumber, int duration) {
-		//TODO remove sysos
-		System.out.println("d " + duration);
-
 		if(toNumber == null) {
 			throw new IllegalArgumentException("toNumber cannot be null");
 		} else if ( duration <= 0) {
@@ -23,7 +20,6 @@ final class UnlimitedAccount extends PhoneAccountFactory {
 			balanceOverBoundary = 0,
 			timeOverBoundary = 0,
 			tempDuration = duration;
-		final Call call;
 		
 		if(bal > 1500) {
 			tempCost = 10 * tempDuration;
@@ -31,15 +27,9 @@ final class UnlimitedAccount extends PhoneAccountFactory {
 			if(bal - tempCost < 1500) {
 				balanceOverBoundary = bal - 1500;
 				timeOverBoundary = balanceOverBoundary / 10;
-				System.out.println("time over 15: " + timeOverBoundary);
-				System.out.println("balance over 15: " + balanceOverBoundary);
-				
 				cost += balanceOverBoundary;
 				bal -= balanceOverBoundary;
 				tempDuration -= timeOverBoundary;
-				
-				System.out.println("Duration mins left " + tempDuration);
-
 			} else {
 				cost += tempCost;
 			}
@@ -51,15 +41,10 @@ final class UnlimitedAccount extends PhoneAccountFactory {
 			if(bal - tempCost < 0 ) {
 				balanceOverBoundary = bal;
 				timeOverBoundary = balanceOverBoundary / 20;
-				System.out.println("time over 0: " + timeOverBoundary);
-				System.out.println("balance over 0: " + balanceOverBoundary);
 				
 				cost += balanceOverBoundary;
 				bal -= balanceOverBoundary;
 				tempDuration -= timeOverBoundary;
-				
-				System.out.println("Duration mins left " + tempDuration);
-	
 			} else {
 				cost += tempCost;
 			}
@@ -70,7 +55,6 @@ final class UnlimitedAccount extends PhoneAccountFactory {
 			cost += tempCost;
 		}
 		debit(cost);
-		System.out.println("Cost: " + cost + " Balance = "  + getBalance());
 		return new Call(toNumber, duration, cost);
 	}
 	

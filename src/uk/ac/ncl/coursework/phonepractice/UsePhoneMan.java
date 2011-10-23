a7e585d850e9a1e09088d5a352c6d25cd7ae7888
jonnyboris <jonny@googlemail.com>
2011-10-23 18:42:55 +0100

    more crude testing

--- src/uk/ac/ncl/coursework/phonepractice/UsePhoneMan.java
+++ src/uk/ac/ncl/coursework/phonepractice/UsePhoneMan.java
@@ -1,19 +1,19 @@
 package uk.ac.ncl.coursework.phonepractice;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class UsePhoneMan {
 	public static void main(String args[]) {
-		 
-		PhoneAccount pa = PhoneAccountFactory.getInstance(new PhoneNumber(191, 2815532), new Person(), PhoneAccountFactory.UNLIMITED);
-		//System.out.println(pa.getBalance());
-		pa.credit(30);
+		Calendar dob  = Calendar.getInstance();
+		dob.set(1988, 10, 20);
+		
+		PhoneAccount pa = PhoneAccountFactory.getInstance(new PhoneNumber(191, 2815532), new Person("Jonathan Fairfull", new Date(dob.getTimeInMillis())), PhoneAccountFactory.UNLIMITED);
+		System.out.println(pa.credit(29));
 		System.out.println(pa.getBalance());
-		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
-		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
-		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
-		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
-		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
-		pa.chargeCall(new PhoneNumber(191, 2813255), 75);
-		//System.out.println(pa.getBalance());
+		
+		System.out.println(pa.credit(30));
+		System.out.println(pa.getBalance());
 	}
 
 }
